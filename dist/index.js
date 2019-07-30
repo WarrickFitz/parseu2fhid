f0cea3564cac03c380384a5a31bb00753d9bf47d
Warrick FitzGerald <warrick@eosnewyork.io>
2019-07-30 13:59:34 -0400

    Init completed

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,39 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
@@ -11,54 +46,68 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var HID = __importStar(require("node-hid"));
-var ref = __importStar(require("ref"));
-var ref_struct_1 = __importDefault(require("ref-struct"));
-// define the "timeval" struct type
-var U2FHID_INIT_RESPONSE = ref_struct_1.default({
-    channelID: ref.types.uint,
-    cmd: ref.types.uchar,
-    cmd2: ref.types.uchar,
-    bcnt: ref.types.short,
-    nonce: ref.types.uint64,
-    cid: ref.types.uint,
-    versionInterface: ref.types.uchar,
-    versionMajor: ref.types.uchar,
-    versionMinor: ref.types.uchar,
-    versionBuild: ref.types.uchar,
-    capFlags: ref.types.uchar // Capabilities flags      
-});
+var p_event_1 = __importDefault(require("p-event"));
+var crypto_1 = __importDefault(require("crypto"));
 var uDevice = /** @class */ (function () {
     function uDevice() {
+        this.initResponse = { broadcastChannelID: -1, cmd: -1, bcnt: -1, nonce: Buffer.allocUnsafe(8).fill('!'), channelID: -1, versionInterface: -1, versionMajor: -1, versionMinor: -1, versionBuild: -1, capFlags: -1 };
         this.device = new HID.HID(4292, 35535);
-        this.device.on("data", this.showResponse);
+        this.handleInitResponse = this.handleInitResponse.bind(this);
+        this.printInitResponseData = this.printInitResponseData.bind(this);
+        this.device.on("data", this.handleInitResponse);
     }
-    uDevice.prototype.showResponse = function (data) {
-        console.log("data happened x:");
-        console.log(data);
-        var x = ref.get(data, 0, U2FHID_INIT_RESPONSE);
-        //@ts-ignore
-        var p = x;
-        console.log("------");
-        console.log("channelID: " + p.channelID.toString(16));
-        console.log("cmd: " + p.cmd.toString(16));
-        console.log("bcnt: " + p.bcnt.toString(16) + ' / ' + p.bcnt);
-        console.log("nonce: " + p.nonce.toString(16));
-        console.log("cid : " + p.cid.toString(16));
-        console.log("versionInterface : " + p.versionInterface.toString(16));
-        console.log("versionMajor : " + p.versionMajor.toString(16));
-        console.log("versionMinor : " + p.versionMinor.toString(16));
-        console.log("versionBuild : " + p.versionBuild.toString(16));
-        console.log("capFlags : " + p.capFlags.toString(16));
+    uDevice.prototype.handleInitResponse = function (data) {
+        this.initResponse.broadcastChannelID = data.readUInt32BE(0);
+        this.initResponse.cmd = data.readUInt8(4);
+        this.initResponse.bcnt = data.readUInt16BE(5);
+        data.copy(this.initResponse.nonce, 0, 7);
+        var remainingData = data.slice(7);
+        this.initResponse.channelID = remainingData.readUInt32BE(8);
+        this.initResponse.versionInterface = remainingData.readUInt8(12);
+        this.initResponse.versionMajor = remainingData.readUInt8(13);
+        this.initResponse.versionMinor = remainingData.readUInt8(14);
+        this.initResponse.versionBuild = remainingData.readUInt8(15);
+        this.initResponse.capFlags = remainingData.readUInt8(16);
     };
-    uDevice.prototype.getChannelID = function () {
-        this.device.write([0xff, 0xff, 0xff, 0xff, 0xff, 0x86, 0x00, 0x08, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01]);
-        // this.device.write([0xff,0xff,0xff,0xff,0xff,0x86,0x00,0x08,0x05,0xb6,0xe8,0xb0,0x83,0x9c,0x47,0x83]);
+    uDevice.prototype.printInitResponseData = function () {
+        console.log("------");
+        console.log("broadcastChannelID: " + this.initResponse.broadcastChannelID.toString(16));
+        console.log("cmd: " + this.initResponse.cmd.toString(16));
+        console.log("bcnt: " + this.initResponse.bcnt.toString(16) + ' / ' + this.initResponse.bcnt);
+        console.log("nonce: " + this.initResponse.nonce.toString('hex'));
+        console.log("channelID : " + this.initResponse.channelID.toString(16));
+        console.log("versionInterface : " + this.initResponse.versionInterface.toString(16));
+        console.log("versionMajor : " + this.initResponse.versionMajor.toString(16));
+        console.log("versionMinor : " + this.initResponse.versionMinor.toString(16));
+        console.log("versionBuild : " + this.initResponse.versionBuild.toString(16));
+        console.log("capFlags : " + this.initResponse.capFlags.toString(16));
+    };
+    uDevice.prototype.init = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var windowsChomp, broadcastChannelID, cmd, length, nonce, result;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        windowsChomp = [0xff];
+                        broadcastChannelID = [0xff, 0xff, 0xff, 0xff];
+                        cmd = [0x86];
+                        length = [0x00, 0x08];
+                        nonce = Array.from(crypto_1.default.pseudoRandomBytes(8));
+                        // this.device.write([0xff,0xff,0xff,0xff,0xff,0x86,0x00,0x08,0x05,0xb6,0xe8,0xb0,0x83,0x9c,0x47,0x83]);
+                        this.device.write(windowsChomp.concat(broadcastChannelID).concat(cmd).concat(length).concat(nonce));
+                        return [4 /*yield*/, p_event_1.default(this.device, 'data')];
+                    case 1:
+                        result = _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
     };
     return uDevice;
 }());
 var d = new uDevice();
-d.getChannelID();
-function alertFunc() {
-    console.log('done');
-}
-setTimeout(alertFunc, 3000);
+d.init().then(function () {
+    d.printInitResponseData();
+    console.log("Done.");
+    process.exit(1);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,67 +1,85 @@
 import * as HID from 'node-hid';
-import * as ref from 'ref';
-import StructType from 'ref-struct';
+import pEvent from 'p-event';
+import crypto from 'crypto';
+import { exists } from 'fs';
 
-// define the "timeval" struct type
-let U2FHID_INIT_RESPONSE = StructType({
-    channelID : ref.types.uint,
-    cmd : ref.types.uchar,
-    cmd2 : ref.types.uchar,
-    bcnt : ref.types.short,
-    nonce: ref.types.uint64,
-    cid : ref.types.uint,		// Channel identifier  
-    versionInterface : ref.types.uchar,	// Interface version
-    versionMajor : ref.types.uchar,	// Major version number
-    versionMinor : ref.types.uchar,	// Minor version number
-    versionBuild : ref.types.uchar,	// Build version number
-    capFlags : ref.types.uchar		// Capabilities flags      
-})
-
+interface U2FHID_INIT_RESPONSE {
+    broadcastChannelID : number;
+    cmd  : number;
+    bcnt : number;
+    nonce : Buffer;
+    channelID : number;
+    versionInterface : number;
+    versionMajor : number;
+    versionMinor : number;
+    versionBuild : number;
+    capFlags : number;
+}
 
 class uDevice {
     device: any;
+    initResponse : U2FHID_INIT_RESPONSE = { broadcastChannelID: -1, cmd: -1,bcnt :  -1, nonce:  Buffer.allocUnsafe(8).fill('!'), channelID: -1, versionInterface: -1, versionMajor: -1, versionMinor: -1, versionBuild: -1, capFlags: -1  } ;
 
     constructor() {
         this.device = new HID.HID(4292,35535);
-        this.device.on("data", this.showResponse);
+
+        this.handleInitResponse = this.handleInitResponse.bind(this);
+        this.printInitResponseData = this.printInitResponseData.bind(this);
+        this.device.on("data", this.handleInitResponse);
     }
 
-    showResponse(data: any) {
-        console.log("data happened x:");
-        console.log(data);
+    handleInitResponse(data: Buffer) {
+        this.initResponse.broadcastChannelID = data.readUInt32BE(0);
+        this.initResponse.cmd = data.readUInt8(4);
+        this.initResponse.bcnt = data.readUInt16BE(5);
 
-        let x = ref.get(data, 0, U2FHID_INIT_RESPONSE);
-        //@ts-ignore
-        let p : U2FHID_INIT_RESPONSE = (x as U2FHID_INIT_RESPONSE);
+        data.copy(this.initResponse.nonce, 0, 7);
+
+        let remainingData = data.slice(7);
+        this.initResponse.channelID = remainingData.readUInt32BE(8);
+        this.initResponse.versionInterface = remainingData.readUInt8(12);
+        this.initResponse.versionMajor = remainingData.readUInt8(13);
+        this.initResponse.versionMinor = remainingData.readUInt8(14);
+        this.initResponse.versionBuild = remainingData.readUInt8(15);
+        this.initResponse.capFlags = remainingData.readUInt8(16);        
+    }
+
+    printInitResponseData() {
         console.log("------");
-        console.log("channelID: " + p.channelID.toString(16));
-        console.log("cmd: " + p.cmd.toString(16));
-        console.log("bcnt: " + p.bcnt.toString(16) + ' / ' + p.bcnt);
-        console.log("nonce: " + p.nonce.toString(16));        
-        console.log("cid : " +  p.cid.toString(16));
-        console.log("versionInterface : " +  p.versionInterface.toString(16));
-        console.log("versionMajor : " +  p.versionMajor.toString(16));
-        console.log("versionMinor : " +  p.versionMinor.toString(16));
-        console.log("versionBuild : " +  p.versionBuild.toString(16));
-        console.log("capFlags : " +  p.capFlags.toString(16));
+        console.log("broadcastChannelID: " + this.initResponse.broadcastChannelID.toString(16));
+        console.log("cmd: " + this.initResponse.cmd.toString(16));
+        console.log("bcnt: " + this.initResponse.bcnt.toString(16) + ' / ' + this.initResponse.bcnt);
+        console.log("nonce: " + this.initResponse.nonce.toString('hex'));        
+        console.log("channelID : " +  this.initResponse.channelID.toString(16));
+        console.log("versionInterface : " +  this.initResponse.versionInterface.toString(16));
+        console.log("versionMajor : " +  this.initResponse.versionMajor.toString(16));
+        console.log("versionMinor : " +  this.initResponse.versionMinor.toString(16));
+        console.log("versionBuild : " +  this.initResponse.versionBuild.toString(16));
+        console.log("capFlags : " +  this.initResponse.capFlags.toString(16));
     }
 
-    getChannelID() {
-        this.device.write([0xff,0xff,0xff,0xff,0xff,0x86,0x00,0x08,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01]);
+
+    async init() {
+        //TODO: Remember to add an OS test here
+        const windowsChomp = [0xff]; //Windows eats the 1st byte
+        const broadcastChannelID = [0xff,0xff,0xff,0xff]; // FFFFFFFF is the broadcast address 
+        const cmd = [0x86]; // 0x06 is the INIT command with the 7th bit set 
+        const length = [0x00,0x08]; // Will always send an 8 byte nonce
+        // const nonce = [0x05,0xb6,0xe8,0xb0,0x83,0x9c,0x47,0x83];       
+        let nonce : number[] = Array.from(crypto.pseudoRandomBytes(8));
         // this.device.write([0xff,0xff,0xff,0xff,0xff,0x86,0x00,0x08,0x05,0xb6,0xe8,0xb0,0x83,0x9c,0x47,0x83]);
-        
+
+        this.device.write(windowsChomp.concat(broadcastChannelID).concat(cmd).concat(length).concat(nonce));
+        const result = await pEvent(this.device, 'data');
     }
+
 }
 
 
 let d = new uDevice();
-d.getChannelID();
-
-function alertFunc() {
-    console.log('done');
-}
-
-setTimeout(alertFunc, 3000);
-
-
+d.init().then(() => {
+    d.printInitResponseData();
+    console.log("Done.");
+    process.exit(1);
+})
 
